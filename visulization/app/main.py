db11208e2b6035d3cf2032dc34d28fdae8f7ef82
es97 <estellawang0218@gmail.com>
2019-11-21 23:00:31 -0800

    Modified barChart and added another tab into main

--- visulization/app/main.py
+++ visulization/app/main.py
@@ -1,6 +1,9 @@
 # json library
 import json
 
+# Pandas for data management
+import pandas as pd
+
 # os methods for manipulating paths
 from os.path import dirname, join
 
@@ -11,6 +14,7 @@ from bokeh.models.widgets import Tabs
 # Each tab is drawn by one script
 from graphs.sample import sample_tab
 from graphs.network import network_tab
+from graphs.barChart import barChart_tab
 
 # load data and preprocess data
 data_source = join(dirname(__file__), 'data', 'sample.json')
@@ -20,9 +24,10 @@ with open(data_source, "r") as read_file:
 # Create each of the tabs: network, table
 tab0 = sample_tab(data)
 tab1 = network_tab(data)
+tab2 = barChart_tab(data)
 
 # Put all the tabs into one application
-tabs = Tabs(tabs = [tab0,tab1])
+tabs = Tabs(tabs=[tab0, tab1, tab2])
 
 # Put the tabs in the current document for display
 curdoc().add_root(tabs)
