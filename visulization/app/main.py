bd56ea96c72c0bf9482b5a1dbe6e758f76a976a3
Frank Yan <frankyan1129@gmail.com>
2019-11-24 12:51:21 -0800

    add new position tab

--- visulization/app/main.py
+++ visulization/app/main.py
@@ -9,17 +9,17 @@ from bokeh.io import curdoc
 from bokeh.models.widgets import Tabs
 
 # Each tab is drawn by one script
-from graphs.sample import sample_tab
 from graphs.network import network_tab
+from graphs.postion import position_tab
 
 # load data and preprocess data
-data_source = join(dirname(__file__), 'data', 'sample.json')
+data_source = join(dirname(__file__), 'data', 'output.json')
 with open(data_source, "r") as read_file:
     data = json.load(read_file)
 
 # Create each of the tabs: network, table
-tab0 = sample_tab(data)
-tab1 = network_tab(data)
+tab0 = network_tab(data)
+tab1 = position_tab(data)
 
 # Put all the tabs into one application
 tabs = Tabs(tabs = [tab0,tab1])
