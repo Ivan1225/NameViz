a7918f78d3786fad081359b0c2c0d8ed05b80b07
Frank Yan <frankyan1129@gmail.com>
2019-11-24 17:08:38 -0800

    add script

--- nameViz.sh
+++ nameViz.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-echo "Building the DSL"
+echo "Starting NamViz"
 
-echo "Starting WTDSL"
-
-if [ "$1" != "" ]; then   
+if [ "$1" != "" ]; then 
+    echo "Starting Analysis"  
     cd analysis
+    python3 search.py -d $1 -o ../visulization/app/data/output.json
+    cd ../visulization
+    echo "Starting Visualization"  
+    bokeh serve --show app
 else
-    echo "Must provied the target file"
+    echo "Must provied the target project"
 fi
